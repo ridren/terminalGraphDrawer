1b3fdfbdafa923961a6c6954ac2e41426dd47456
Ridren <ridren@protonmail.com>
2023-02-05 17:58:53 +0100

    fixed readme, invalid key for delete part

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char* argv[])
 			             "\tspace/enter - (un)selects part (first searches for box, then for line then for text in cursor pos)\n"
 		                 "\t              \tbox and text is selected by top left corner while line is selected wherever its points are\n"
 		                 "\t              \t`space` doesnt work for unselecting text, enter has to be used\n"
-			             "\td           - removes selected part\n"
+			             "\tTAB         - removes selected part\n"
 	
 			             "\tb           - create box\n"
 			             "\tl           - create line\n"
